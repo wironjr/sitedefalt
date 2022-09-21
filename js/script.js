4fed8cba5c616d8d03d6f6e00a6792ee1248556f
wironjr <jun1n_@live.com>
2022-09-21 20:32:42 -0300

    subir

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/********** animação ********/
+function reveal1() {
+  var reveals = document.querySelectorAll(".reveal1");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal1);
+
+
+function reveal2() {
+  var reveals = document.querySelectorAll(".reveal2");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal2);
+
+
+/********** alerta **************/
+
+function aleta() {
+            alert("\nGW SOFTWARE\nAinda vamos implementar esse botão!")
+        }
+
